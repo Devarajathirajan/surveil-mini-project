678ce196108951f9ebeeb61746809f9835b62eff
devarajathirajan <gokulrajan01234@gmail.com>
2022-04-07 14:19:48 +0530

    used callbackFlow -fixed classroom return issue

--- app/src/main/java/com/crayosa/surveil/fragments/HomeFragment.kt
+++ app/src/main/java/com/crayosa/surveil/fragments/HomeFragment.kt
@@ -2,14 +2,14 @@ package com.crayosa.surveil.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import com.bumptech.glide.Glide
 import com.crayosa.surveil.LoginActivity
 import com.crayosa.surveil.R
@@ -20,6 +20,8 @@ import com.crayosa.surveil.repository.FirebaseRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -29,29 +31,48 @@ class HomeFragment : Fragment() {
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         val auth = FirebaseAuth.getInstance()
-        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,R.layout.fragment_home,container,false)
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater,
+            R.layout.fragment_home,
+            container,
+            false
+        )
 
-        if(auth.currentUser != null){
+        if (auth.currentUser != null) {
             val photoUrl = auth.currentUser!!.photoUrl
             Glide.with(requireContext()).load(photoUrl).into(binding.displayPic)
             binding.displayPic.setOnClickListener {
                 auth.signOut()
                 startActivity(Intent(requireContext(), LoginActivity::class.java))
             }
-            FirebaseRepository(Firebase.firestore)
-                .getEnrolledRooms(auth.currentUser!!.uid)
-        }
+            lifecycleScope.launch {
+                FirebaseRepository(Firebase.firestore)
+                    .getEnrolledRooms(auth.currentUser!!.uid).collectLatest {
+                        Log.d(TAG,it.toString())
+                    }
+            }
 
-        val list = mutableListOf<ClassRoom>(
-            ClassRoom("0","CSE IIIrd year", "Professional English", "Mr. Vivekanand N", emptyList()),
-            ClassRoom("1","CSE IIIrd year", "Cyber Security", "Mr. Ezhumalai P", emptyList())
-        )
-        val adapter = ClassRoomListAdapter()
-        adapter.submitList(list)
-        binding.classRoomList.adapter = adapter
-        binding.createRoom.setOnClickListener {
-            requireView().findNavController().navigate(R.id.action_homeFragment_to_createClassRoom)
+            val list = mutableListOf<ClassRoom>(
+                ClassRoom(
+                    "0",
+                    "CSE IIIrd year",
+                    "Professional English",
+                    "Mr. Vivekanand N",
+                    emptyList()
+                ),
+                ClassRoom("1", "CSE IIIrd year", "Cyber Security", "Mr. Ezhumalai P", emptyList())
+            )
+            val adapter = ClassRoomListAdapter()
+            adapter.submitList(list)
+            binding.classRoomList.adapter = adapter
+            binding.createRoom.setOnClickListener {
+                requireView().findNavController()
+                    .navigate(R.id.action_homeFragment_to_createClassRoom)
+            }
         }
         return binding.root
+    }
+    companion object{
+        const val TAG = "HomeFragment"
     }
 }
--- app/src/main/java/com/crayosa/surveil/repository/FirebaseRepository.kt
+++ app/src/main/java/com/crayosa/surveil/repository/FirebaseRepository.kt
@@ -1,18 +1,35 @@
 package com.crayosa.surveil.repository
 
+import android.util.Log
 import com.crayosa.surveil.datamodels.ClassRoom
+import com.crayosa.surveil.fragments.HomeFragment
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 
 
 class FirebaseRepository(private val firestore: FirebaseFirestore){
-    fun getEnrolledRooms(uID : String) : List<String>{
-        lateinit var result : MutableList<String>
-         firestore.collection(USERS_COLLECTION).document(uID).get()
-            .addOnSuccessListener {
-                result = it.data?.get(ENROLLED_CLASSROOMS) as MutableList<String>
-            }
-        return emptyList()
+
+    fun getEnrolledRooms(uID: String) : Flow<MutableList<String>>{
+        return callbackFlow<MutableList<String>>{
+            var result : MutableList<String>? = mutableListOf()
+            firestore.collection(USERS_COLLECTION).document(uID).get()
+                .addOnSuccessListener {
+                    val data = it.data?.get(ENROLLED_CLASSROOMS)
+                    if (data != null) {
+                        result = data as MutableList<String>
+                        Log.d(HomeFragment.TAG, result.toString())
+                    }
+                    else{
+                        return@addOnSuccessListener
+                    }
+                    trySend(result!!).isSuccess
+                }
+
+            awaitClose {  }
+        }
     }
 
     fun addClassRoom(classroom : ClassRoom, uID: String){
